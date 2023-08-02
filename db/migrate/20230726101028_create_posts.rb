f36c8377ebc9f1165909d2273417d5bb3aff5e74
Amit-Patidar13 <amitpatidar1310@gmail.com>
2023-08-02 18:43:38 +0530

    some required changes

--- db/migrate/20230726101028_create_posts.rb
+++ db/migrate/20230726101028_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.string :name
-      t.string :status, default: "pending" 
+      t.string :status
       t.text :description
       t.references :user, null: false, foreign_key: true
 
